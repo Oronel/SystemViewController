f688b4ea3cafed476e6a066821e5f00c4666855e
Lulu <orone.l@hotmail.fr>
2017-12-10 14:34:17 +0200

    add IBOutlet for imageView and IBActon for button

--- SystemViewController/Controllers/ViewController.swift
+++ SystemViewController/Controllers/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func shareButton(_ sender: Any) {
     }
-
-
+    
+    @IBAction func safariButton(_ sender: Any) {
+    }
+    
+    @IBAction func cameraButton(_ sender: Any) {
+    }
+    
+    @IBAction func emaiButton(_ sender: Any) {
+    }
+    
 }
 
